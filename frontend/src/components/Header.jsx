371c555cc22ddb735663aa8a12f7999171cad47d
assia769 <ahoubbadi139@gmail.com>
2025-04-25 16:55:15 +0100

    assia

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
 // frontend/src/components/Header.jsx
 import React from 'react';
-
 const Header = () => {
   return (
     <header className="dashboard-header">
       <div className="container">
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
           <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src="/logoWeb.png" alt="Logo" style={{ height: '80px' }} />
+
             <h1>Dashboard Recettes Marocaines</h1>
           </div>
         </div>
